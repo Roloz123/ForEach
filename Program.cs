a0f258b2ffa23d39dc6d3ca92472cb04a1645e74
malil6384 <malil6384@19PG0Q2>
2022-09-16 12:56:19 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ForEach";
+            string[] websites = new string[5] { "Google", "YouTube", "FaceBook", "Baidu", "Yahoo!" };
+            Console.WriteLine("Popular Websites...");
+            int rank = 1;
+            foreach(string site in websites)
+            {
+                Console.WriteLine("Position: " + rank + "\t" + site);
+                rank++;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
